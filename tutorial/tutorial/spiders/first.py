7b9fb537ba52c97aa956577261c979caf8e4eab3
lsenhance <1396546312@qq.com>
2018-03-26 21:26:26 +0800

    delete something

--- tutorial/tutorial/spiders/first.py
+++ tutorial/tutorial/spiders/first.py
@@ -8,15 +8,8 @@ class AuthorSpider(scrapy.Spider):
 
     def parse(self,response):
         num=response.xpath('/html/body/div/div/div[2]/h3/text()').extract()[0]
-        print(type(num))
         print(num)
         num=re.sub("\D","",num)
-        print(type(num))
-        print(num)
-        if num is not None:
-            next_page='http://www.heibanke.com/lesson/crawler_ex00/'+ num
-            if next_page is not None:
-                yield scrapy.Request(next_page, callback=self.parse)
-        else:
-            print('good work')
+        next_page='http://www.heibanke.com/lesson/crawler_ex00/'+ num
+        yield scrapy.Request(next_page, callback=self.parse)
     
