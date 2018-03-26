2ad662ad406b250065311cea991caefb1f266fa2
lsenhance <1396546312@qq.com>
2018-03-26 10:01:07 +0800

    first commit

---
+++ tutorial/tutorial/spiders/first.py
@@ -0,0 +1,19 @@
+import scrapy
+import re
+
+class AuthorSpider(scrapy.Spider):
+    name = 'first'
+    start_urls=(
+        'http://www.heibanke.com/lesson/crawler_ex00/',)
+
+    def parse(self,response):
+        num=response.xpath('/html/body/div/div/div[2]/h3/text()').extract()[0]
+        print(type(num))
+        print(num)
+        num=re.sub("\D","",num)
+        print(type(num))
+        print(num)
+        next_page='http://www.heibanke.com/lesson/crawler_ex00/'+ num
+        if next_page is not None:
+            yield scrapy.Request(next_page, callback=self.parse)
+    
